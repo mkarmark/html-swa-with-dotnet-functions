60af6e341db987ced7cf1867aa3e8ce4676b1cf9
mkarmark <mikarmar@microsoft.com>
2023-12-08 13:45:28 -0800

    Update WeatherForecastFunction.cs

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -23,7 +23,7 @@ namespace Api
             var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
+                TemperatureC = temp = randomNumber.Next(-30, 55),
                 Summary = GetSummary(temp)
             }).ToArray();
 
